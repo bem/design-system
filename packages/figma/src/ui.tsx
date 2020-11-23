f034ee7ca0e797dbc7df2283f3e8e2d72404a604
Pavel <jokeme83@gmail.com>
2020-11-23 16:22:49 +0300

    chore: initial commit

---
+++ packages/figma/src/ui.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+const App = () => {
+  return <h1>Yandex Design System</h1>
+}
+
+ReactDom.render(<App />, document.getElementById('root'))
